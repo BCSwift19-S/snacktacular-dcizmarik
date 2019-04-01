9f80275c122266a57d3ead3ffb6b83ea92fad270
Daniel Cizmarik <dcizmarik@daniels-macbook-pro-3.local>
2019-04-01 13:05:34 -0400

    Initial Commit

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,15 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Daniel Cizmarik on 4/1/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    
+    var spotArray = [Spot]()
+    
+}
